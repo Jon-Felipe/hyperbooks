292de5d50719d7a405c448287c3d8e8a8c833d05
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-20 19:27:54 +0100

    dummy books data

---
+++ src/utils/constants.ts
@@ -0,0 +1,46 @@
+import { BookType } from './types';
+
+export const dummy_books: BookType[] = [
+  {
+    id: 1,
+    name: 'Work for Money, Design for Love',
+    image: '',
+    category: 'Business & Money',
+    price: 22.0,
+  },
+  {
+    id: 2,
+    name: 'Very Nice: A novel Marcy Dermansky',
+    image: '',
+    category: 'Literature & Fiction',
+    price: 18.9,
+  },
+  {
+    id: 3,
+    name: 'The Psychology of Graphic Design Pricing',
+    image: '',
+    category: 'Graphic Design',
+    price: 20.59,
+  },
+  {
+    id: 4,
+    name: 'Juliet the Maniac: A Novel',
+    image: '',
+    category: 'Literature & Fiction',
+    price: 9.99,
+  },
+  {
+    id: 5,
+    name: 'Logo Design Love: A Guide to Creating Iconic Brand Identities',
+    image: '',
+    category: 'Graphic Design',
+    price: 24.91,
+  },
+  {
+    id: 6,
+    name: 'Thinking with Type, 2nd revised and expanded',
+    image: '',
+    category: 'Graphic Design',
+    price: 12.04,
+  },
+];
