ee7e120158fa08de6de90c5b8b9e5ffd378a2d94
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-20 20:04:55 +0100

    displaying books in grid view

--- src/components/BookGridView.tsx
+++ src/components/BookGridView.tsx
@@ -1,7 +1,43 @@
-type Props = {};
+// mui
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
-function BookGridView({}: Props) {
-  return <div>BookGridView</div>;
+// extra
+import { dummy_books } from '../utils/constants';
+
+function BookGridView() {
+  return (
+    <Grid container spacing={2}>
+      {dummy_books.map((book) => (
+        <Grid key={book.id} item xs={12} md={6}>
+          <Card variant='outlined'>
+            <CardContent>
+              <Typography variant='body1' component='h3' noWrap>
+                {book.name}
+              </Typography>
+              <Typography variant='body2' component='p'>
+                {book.category}
+              </Typography>
+              <Typography variant='caption' component='p'>
+                ${book.price.toFixed(2)}
+              </Typography>
+              <Button variant='outlined' size='small'>
+                Add To Cart
+              </Button>
+              <IconButton size='small' sx={{ ml: 1 }}>
+                <FavoriteBorderOutlinedIcon fontSize='small' />
+              </IconButton>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default BookGridView;
