b93152f604e67cf033c46f199c4897bf57da1c6d
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-20 21:39:15 +0100

    search filter text input template

--- src/components/SideFilters.tsx
+++ src/components/SideFilters.tsx
@@ -1,7 +1,29 @@
+// mui
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
 type Props = {};
 
 function SideFilters({}: Props) {
-  return <div>SideFilters</div>;
+  return (
+    <Paper sx={{ padding: '20px' }}>
+      {/* Search Filter */}
+      <TextField
+        placeholder='Title, Author...'
+        variant='standard'
+        fullWidth
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position='end'>
+              <SearchOutlinedIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Paper>
+  );
 }
 
 export default SideFilters;
