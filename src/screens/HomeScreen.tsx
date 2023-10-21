3eeaddd9d158d11c713dcba70dc6d8bb4e403ef4
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-21 10:38:40 +0100

    toggle book view buttons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,6 +4,11 @@ import BookGridView from '../components/BookGridView';
 
 // mui
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import ToggleButton from '@mui/material/ToggleButton';
+import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
+import MenuIcon from '@mui/icons-material/Menu';
 
 type Props = {};
 
@@ -15,6 +20,22 @@ function HomeScreen({}: Props) {
           <SideFilters />
         </Grid>
         <Grid item xs={12} md={9}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              marginBottom: '20px',
+            }}
+          >
+            <ToggleButtonGroup>
+              <ToggleButton value={''}>
+                <GridViewRoundedIcon />
+              </ToggleButton>
+              <ToggleButton value={''}>
+                <MenuIcon />
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
           <BookGridView />
         </Grid>
       </Grid>
