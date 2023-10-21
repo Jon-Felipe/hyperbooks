de56b6cacdfa7f9ab05fa96d1ff0baab8b01488e
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-21 09:02:35 +0100

    display card book image

--- src/components/BookGridView.tsx
+++ src/components/BookGridView.tsx
@@ -2,6 +2,7 @@
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import Button from '@mui/material/Button';
@@ -10,15 +11,22 @@ import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlin
 
 // extra
 import { dummy_books } from '../utils/constants';
+import bookImg from '../assets/book2.jpg';
 
 function BookGridView() {
   return (
     <Grid container spacing={2}>
       {dummy_books.map((book) => (
         <Grid key={book.id} item xs={12} md={6}>
-          <Card variant='outlined'>
+          <Card variant='outlined' sx={{ display: 'flex', height: 200 }}>
+            <CardMedia
+              component='img'
+              image={bookImg}
+              sx={{ width: 151 }}
+              alt={book.title}
+            />
             <CardContent>
-              <Typography variant='body1' component='h3' noWrap gutterBottom>
+              <Typography variant='body1' component='h3' gutterBottom>
                 {book.title}
               </Typography>
               <Typography variant='body2' component='p' gutterBottom>
