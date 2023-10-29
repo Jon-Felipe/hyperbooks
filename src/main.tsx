8b12bf0c871536ba106e3bc613f49ecd5e8df33c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-29 18:19:01 +0000

    redux store setup

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,8 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.ts';
 
 // pages
 import HomeScreen from './screens/HomeScreen.tsx';
@@ -28,6 +30,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
