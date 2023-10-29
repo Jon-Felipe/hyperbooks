2cf4caacd18c5ff30ef369a49b3506bf4a906e8c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-29 21:06:14 +0000

    setup react toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // components
 import Header from './components/Header';
@@ -9,6 +11,7 @@ import Container from '@mui/material/Container';
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <Container component='main' maxWidth='xl' sx={{ marginTop: '20px' }}>
         <Outlet />
