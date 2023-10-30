f50b1a1fd3ce5750a5e0d1793af5fa54662b880f
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-30 20:50:20 +0000

    separated code to cart summary component

---
+++ src/components/CartSummary.tsx
@@ -0,0 +1,78 @@
+// mui
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+type Props = {};
+
+function CartSummary({}: Props) {
+  return (
+    <Grid container spacing={4} sx={{ marginTop: '20px' }}>
+      {/* cart items */}
+      <Grid item md={8}>
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Quantity</TableCell>
+                <TableCell>Subtotal</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell component='th' scope='row'>
+                  The Overdue Life of Amy Byler
+                </TableCell>
+                <TableCell>$37.00</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>$74.00</TableCell>
+                <TableCell>x </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      {/* cart total summary */}
+      <Grid item md={4}>
+        <Typography component='h3' variant='h5'>
+          Cart Totals
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography component='h6' variant='body1' sx={{ marginTop: '10px' }}>
+            Subtotal
+          </Typography>
+          <Typography component='p' variant='body2'>
+            $74.00
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography component='h6' variant='body1' sx={{ marginTop: '10px' }}>
+            Shipping
+          </Typography>
+          <Typography component='p' variant='body2'>
+            Free Shipping
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography component='h6' variant='body1' sx={{ marginTop: '10px' }}>
+            Total
+          </Typography>
+          <Typography component='p' variant='body2'>
+            $74.00
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default CartSummary;
--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,18 +1,13 @@
 import { useState } from 'react';
 
+// components
+import CartSummary from '../components/CartSummary';
+
 // mui
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 
 const steps = ['Cart Summary', 'Checkout & Pay', 'Order Summary'];
 
@@ -32,77 +27,7 @@ function CartScreen({}: Props) {
             </Step>
           ))}
         </Stepper>
-        <Grid container spacing={4} sx={{ marginTop: '20px' }}>
-          {/* cart items */}
-          <Grid item md={8}>
-            <TableContainer>
-              <Table sx={{ minWidth: 650 }}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Product</TableCell>
-                    <TableCell>Price</TableCell>
-                    <TableCell>Quantity</TableCell>
-                    <TableCell>Subtotal</TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell component='th' scope='row'>
-                      The Overdue Life of Amy Byler
-                    </TableCell>
-                    <TableCell>$37.00</TableCell>
-                    <TableCell>2</TableCell>
-                    <TableCell>$74.00</TableCell>
-                    <TableCell>x </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Grid>
-          {/* cart total summary */}
-          <Grid item md={4}>
-            <Typography component='h3' variant='h5'>
-              Cart Totals
-            </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography
-                component='h6'
-                variant='body1'
-                sx={{ marginTop: '10px' }}
-              >
-                Subtotal
-              </Typography>
-              <Typography component='p' variant='body2'>
-                $74.00
-              </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography
-                component='h6'
-                variant='body1'
-                sx={{ marginTop: '10px' }}
-              >
-                Shipping
-              </Typography>
-              <Typography component='p' variant='body2'>
-                Free Shipping
-              </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography
-                component='h6'
-                variant='body1'
-                sx={{ marginTop: '10px' }}
-              >
-                Total
-              </Typography>
-              <Typography component='p' variant='body2'>
-                $74.00
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
+        <CartSummary />
       </Box>
     </>
   );
