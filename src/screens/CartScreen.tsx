bcc65f996e72c512a5a36082d2218080e2b58807
Jon-Felipe <rodriguesjf7@gmail.com>
2023-10-30 08:59:32 +0000

    cart screen stepper initial

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+
+// mui
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const steps = ['Shopping Summary', 'Checkout', 'Order Summary'];
+
 type Props = {};
 
 function CartScreen({}: Props) {
-  return <div>CartScreen</div>;
+  const [activeStep, setActiveStep] = useState<number>(0);
+
+  return (
+    <>
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((step, index) => (
+            <Step key={index}>
+              <StepLabel>{step}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+    </>
+  );
 }
 
 export default CartScreen;
